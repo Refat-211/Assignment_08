352e6d085bfb427d77239e2e13a791541a0cf9ad
unknown <refathassan512@gmail.com>
2022-09-29 13:29:16 +0600

    exercise six cards updated

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -5,8 +5,17 @@ const Exercise = (props) => {
     const {img, title, time} = props.exercise;
     return (
         <div className='exercise'>
+            <div>
             <img src={img} alt="" />
+            <h3 className='exercise-title'>{title}</h3>
+            <p className='exercise-age'>For Age: 20-25</p>
+            <p className='exercise-time'>Time required: {time}s</p>
+            </div>
+            <button className='btn-list'>
+                <h4>Add to List</h4>
+            </button>
         </div>
+        
     );
 };
 
